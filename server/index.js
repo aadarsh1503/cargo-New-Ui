604032a1eb3a275ff9dbdb4fd46ede8543704330
aadarsh1503 <aadarshchauhan35@gmail.com>
2026-04-16 17:11:07 +0530

    done

--- server/index.js
+++ server/index.js
@@ -30,7 +30,14 @@ const app = express();
 
 // --- Middleware Setup ---
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://cargo-new-ui.vercel.app',
+    'https://gvscargo.com',
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
